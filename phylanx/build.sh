6798a732f156f09fee5597aee800ce3b5a2294c7
R. Tohid <tohid.rod@gmail.com>
2020-02-26 15:49:26 -0600

    Phylanx: developer environment.

--- phylanx/build.sh
+++ phylanx/build.sh
@@ -1,48 +1,24 @@
 #!/bin/bash
-PROCS=$(lscpu | grep CPU.s.: | head -1 | cut -d: -f2)
-if [ "$USE_PROCS" = "" ]
-then
-  USE_PROCS=$(($PROCS/2))
-fi
-if [ "$1" = "config" ]
-then
-    shift
-    CONFIG=1
-else
-    CONFIG=0
-fi
 
-# Disable parallel testing in the hopes that tests will pass...
-# Ticket https://github.com/STEllAR-GROUP/phylanx/issues/810
-#export CTEST_PARALLEL_LEVEL=${USE_PROCS}
-#export HPX_COMMANDLINE_OPTIONS=--hpx:bind=none
+PROCS=24
+PREFIX=$HOME/bin
+BUILD_TYPE=Debug
+MALLOC=system
 
-# For singularity, prevent these from being over-written
-unset CC
-unset CXX
-cd
-if [ ! -d ~/phylanx ]
-then
-  git clone https://github.com/STEllAR-GROUP/phylanx.git
-fi
-if [ ! -d ~/phylanx/build ]
-then
-    CONFIG=1
-fi
-if [ $CONFIG = 1 ]
-then
-  mkdir -p ~/phylanx/build
-  cd ~/phylanx/build
-  cmake -DCMAKE_BUILD_TYPE=Release \
-      -DCMAKE_INSTALL_PREFIX=$HOME/install/phylanx \
-      -DHPX_DIR=/usr/local/lib64/cmake/HPX \
-      -DPHYLANX_WITH_TESTS_UNIT=on \
-      -DPYTHON_EXECUTABLE=/usr/local/bin/python3 \
-      -DPHYLANX_WITH_CXX17=on \
-      -DPHYLANX_WITH_TOOLS=on \
-      -DBlazeTensor_DIR=/blaze_tensor/build/cmake \
-      ..
-fi
-cd ~/phylanx/build
+mkdir ~/repos/phylanx/build
+cd ~/repos/phylanx/build
+cmake \
+    -Dblaze_DIR=$PREFIX=/blaze/share/blaze/cmake/          			        \
+    -DBlazeTensor_DIR=${PREFIX}/blaze_tensor/share/blaze/cmake/          	\
+    -DPHYLANX_WITH_BLAZE_TENSOR=ON                                          \
+    -Dpybind11_DIR=$PREFIX/pybind11/share/cmake/pybind11/ 			        \
+    -DCMAKE_BUILD_TYPE=$BUILD_TYPE                                          \
+    -DCMAKE_INSTALL_PREFIX=$PREFIX						                    \
+    -DHPX_WITH_MALLOC=$MALLOC                                               \
+    -DHPX_DIR=$PREFIX/hpx                                                   \
+    -DPHYLANX_WITH_VIM_YCM=ON                                               \
+    -Wdev                                                                   \
+    ..
+
+cd ~/repos/phylanx/build
 make -j $USE_PROCS $* 2>&1 | tee make.out
- 
